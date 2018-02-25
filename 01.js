84644142ebf798117901f1587a8dfbd4e55d0065
Bozsanyi <arnold.bozsanyi@capgemini.com>
2018-02-25 12:55:42 +0100

    01.js has been created with the excercises

---
+++ 01.js
@@ -0,0 +1,31 @@
+'user strict';
+
+// 1.
+// Write a method, that takes an argument and logs
+// it to the console. call the method, to check if it works.
+
+// 2.
+// Write a method, that takes 2 aruments, and returns their sum.
+
+// 3.
+// Write a method, that takes an array with 2 numbers, and return value that is
+// the bigger number multiplied by the smaller number. If it has more numbers,
+// return "It has more numbers", if one if the numbers is 0 return "can not multiply by zero"
+// and handle all else possibilities, for example if the array has only one element,
+// it doesnt have only numbers, etc...
+
+// 4.
+// Write a method, that takes a string as an argument, and it should return
+// the string as a number, if its not possible it should return
+// "this is not a number"
+
+// 5.
+// Write a method, that takes an array, and returns that array sorted
+
+// 6.
+// Write a method that takes a text as a string together,
+// and it should return the text well separated. Use only ES5 syntax.
+
+// 7.
+// Since we don't have classes in es5 js, you should create a constructor Person.
+// You should also have a woman and a man constructor, read online about js constructors.
